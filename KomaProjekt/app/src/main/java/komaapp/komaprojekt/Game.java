536b3eef7fe4b3d35c56bc4300bc2a28d786c249
Benjamin Wiberg <benjaminwiberg@gmail.com>
2014-11-10 10:50:03 +0100

    Updated AndEngine to latest GitHub version, added HelloWorld Game activity using AndEngine.

--- KomaProjekt/app/src/main/java/komaapp/komaprojekt/Game.java
+++ KomaProjekt/app/src/main/java/komaapp/komaprojekt/Game.java
@@ -1,40 +1,57 @@
 package komaapp.komaprojekt;
 
-import android.app.ActionBar;
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.graphics.Typeface;
 
-public class Game extends Activity {
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.text.Text;
+import org.andengine.opengl.font.Font;
+import org.andengine.opengl.font.FontFactory;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+public class Game extends SimpleBaseGameActivity {
 
-        //Hides Action Bar
-        ActionBar actionBar = getActionBar();
-        actionBar.hide();
-    }
+    private Camera camera;
+    private static final int CAMERA_WIDTH = 768;
+    private static final int CAMERA_HEIGHT = 1280;
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.huvudmeny, menu);
-        return true;
-    }
+    private Font mFont;
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public EngineOptions onCreateEngineOptions()
+    {
+        camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+        EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED,
+        new FillResolutionPolicy(), camera);
+        return engineOptions;
     }
+
+    @Override
+    protected void onCreateResources()
+    {
+        this.mFont = FontFactory.create(this.getFontManager(), this.getTextureManager(), 256, 256, TextureOptions.BILINEAR, Typeface.create(Typeface.DEFAULT, Typeface.BOLD), 48);
+        this.mFont.load();
+    }
+
+    @Override
+    protected Scene onCreateScene()
+    {
+        final Scene scene = new Scene();
+        scene.setBackground(new Background(0.09804f, 0.6274f, 0.8784f));
+
+        final int text_x = 100;
+        final int text_y = 160;
+
+        final Text helloWorld = new Text(text_x, text_y, this.mFont, "Hello, world!", this.getVertexBufferObjectManager());
+
+        scene.attachChild(helloWorld);
+
+        return scene;
+    }
+
 }
