36646672bcbbd72ca0ff5bc16bdc4cb3970d5b6b
Benjamin Wiberg <benjaminwiberg@gmail.com>
2014-11-12 14:24:39 +0100

    FPS Counter, basic movable sprite implemented. Added Player.java class.

--- KomaProjekt/app/src/main/java/komaapp/komaprojekt/Game.java
+++ KomaProjekt/app/src/main/java/komaapp/komaprojekt/Game.java
@@ -1,22 +1,30 @@
 package komaapp.komaprojekt;
 
 import android.graphics.Typeface;
+import android.util.Log;
 
 import org.andengine.engine.camera.Camera;
 import org.andengine.engine.handler.IUpdateHandler;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
+import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.text.Text;
 import org.andengine.entity.util.FPSCounter;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.font.Font;
 import org.andengine.opengl.font.FontFactory;
+import org.andengine.opengl.texture.ITexture;
 import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 
-public class Game extends SimpleBaseGameActivity {
+public class Game extends SimpleBaseGameActivity implements IOnSceneTouchListener {
 
     private Camera camera;
     private static final int CAMERA_WIDTH = 768;
@@ -24,20 +32,34 @@ public class Game extends SimpleBaseGameActivity {
 
     private Font mFont;
 
+    //TEXTURES
+    private BitmapTextureAtlas texAtlas;
+    private ITextureRegion xWing_tex;
+
+    private Sprite player;
+
     @Override
     public EngineOptions onCreateEngineOptions()
     {
         camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-        EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED,
-        new FillResolutionPolicy(), camera);
+        EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED, new FillResolutionPolicy(), camera);
+        engineOptions.getTouchOptions().setNeedsMultiTouch(true);
         return engineOptions;
     }
 
     @Override
     protected void onCreateResources()
     {
+        //FONTS
         this.mFont = FontFactory.create(this.getFontManager(), this.getTextureManager(), 256, 256, TextureOptions.BILINEAR, Typeface.create(Typeface.DEFAULT, Typeface.BOLD), 48);
         this.mFont.load();
+
+        //GRAPHICS
+        BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+        texAtlas = new BitmapTextureAtlas(getTextureManager(), 473, 513, TextureOptions.DEFAULT);
+        xWing_tex = BitmapTextureAtlasTextureRegionFactory.createFromAsset(texAtlas, this, "xwing_sprite.png", 0, 0);
+        texAtlas.load();
+
     }
 
     @Override
@@ -45,19 +67,32 @@ public class Game extends SimpleBaseGameActivity {
     {
         final Scene scene = new Scene();
 
+        scene.setOnSceneTouchListener(this);
+
         final FPSCounter fpsCounter = new FPSCounter();
         this.mEngine.registerUpdateHandler(fpsCounter);
 
         scene.setBackground(new Background(0.09804f, 0.6274f, 0.8784f));
 
-        final int text_x = 100;
-        final int text_y = 160;
-
-        final Text helloWorld = new Text(text_x, text_y, this.mFont, "Hello, world!", this.getVertexBufferObjectManager());
-        final Text fpsText = new Text(text_x, text_y+100, this.mFont, "FPS: ", "FPS: XXXXX".length(), this.getVertexBufferObjectManager());
-
-        scene.attachChild(helloWorld);
+        final Text fpsText = new Text(10, 10, this.mFont, "FPS: ", "FPS: XXXXX".length(), this.getVertexBufferObjectManager());
         scene.attachChild(fpsText);
+
+        player = new Sprite(300, 200, xWing_tex, this.getVertexBufferObjectManager())
+        {
+            @Override
+            public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float X, float Y)
+            {
+                if (pSceneTouchEvent.isActionDown())
+                {
+                    Log.d("TextLog", "Player sprite touched.");
+                }
+                return true;
+            }
+        };
+
+
+        scene.attachChild(player);
+        scene.registerTouchArea(player);
 
         scene.registerUpdateHandler(new IUpdateHandler() {
             float currentTime = 0;
@@ -67,7 +102,6 @@ public class Game extends SimpleBaseGameActivity {
                 currentTime += v;
 
                 fpsText.setText(String.format("FPS: %.2f", fpsCounter.getFPS()));
-                helloWorld.setPosition(text_x+(int)(60.0*Math.cos(currentTime)), text_y);
             }
 
             @Override
@@ -77,4 +111,20 @@ public class Game extends SimpleBaseGameActivity {
         return scene;
     }
 
+    @Override
+    public boolean onSceneTouchEvent(Scene scene, TouchEvent touchEvent) {
+        if (touchEvent.isActionDown())
+        {
+            //Execute touch event
+            float newX, newY;
+            newX = touchEvent.getX() - player.getWidth()/2;
+            newY = touchEvent.getY() - player.getHeight()/2;
+
+            player.setPosition(newX, newY);
+            Log.d("TextLog", "Screen touched at X=" + touchEvent.getX() + ", Y=" + touchEvent.getY());
+
+        }
+
+        return false;
+    }
 }
---
+++ KomaProjekt/app/src/main/java/komaapp/komaprojekt/Player.java
@@ -0,0 +1,8 @@
+package komaapp.komaprojekt;
+
+/**
+ * Created by benjamin on 14-11-12.
+ */
+public class Player {
+
+}
