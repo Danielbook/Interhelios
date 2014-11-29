16db76361d5f6143a252bce7b2a41f7430655a42
Daniel Böök <Daniel.book@hotmail.com>
2014-11-29 08:16:45 +0100

    Added two shotmanager object, one for player and one for enemies

--- KomaProjekt/app/src/main/java/komaapp/komaprojekt/Game.java
+++ KomaProjekt/app/src/main/java/komaapp/komaprojekt/Game.java
@@ -48,10 +48,11 @@ public class Game extends SimpleBaseGameActivity implements IOnSceneTouchListene
     private MovingBackground background_clouds1,background_clouds2, background_stars;
 
     private Player player;
+    private ShotManager playerShotManager;
 
     //ENEMIES
     private EnemyManager enemyManager;
-    private ShotManager shotManager;
+    private ShotManager enemyShotManager;
 
     //DATABASE
     private Database database = new Database();
@@ -94,8 +95,8 @@ public class Game extends SimpleBaseGameActivity implements IOnSceneTouchListene
                 {
                     //SHOOT
                     Log.d("ShotLog", "Player tried to shoot!");
-                    shotManager.addShot(new Shot(player.getCenterX()-player.getWidth()/2, player.getCenterY(), new Vector2(0f, -1f), 15f, 60f, this.getVertexBufferObjectManager(), 100f, 0, 0));
-                    shotManager.addShot(new Shot(player.getCenterX()+player.getWidth()/2, player.getCenterY(), new Vector2(0f, -1f), 15f, 60f, this.getVertexBufferObjectManager(), 100f, 0, 0));
+                    playerShotManager.addShot(new Shot(player.getCenterX() - player.getWidth() / 2, player.getCenterY(), new Vector2(0f, -1f), 15f, 60f, this.getVertexBufferObjectManager(), 100f, 0, 0));
+                    playerShotManager.addShot(new Shot(player.getCenterX() + player.getWidth() / 2, player.getCenterY(), new Vector2(0f, -1f), 15f, 60f, this.getVertexBufferObjectManager(), 100f, 0, 0));
                 }
                 return true;
             };
@@ -132,11 +133,11 @@ public class Game extends SimpleBaseGameActivity implements IOnSceneTouchListene
         this.mFont = FontFactory.create(this.getFontManager(), this.getTextureManager(), 256, 256, TextureOptions.BILINEAR, Typeface.create(Typeface.DEFAULT, Typeface.BOLD), 48);
         this.mFont.load();
 
-        background_tex_clouds1 = loadITextureRegion("bkgrnd_clouds.png", 1200, 3000);
+        background_tex_clouds1 = loadITextureRegion("bkgrnd_clouds.png", CAMERA_WIDTH, 3000);
 
-        background_tex_clouds2 = loadITextureRegion("bkgrnd_clouds.png", 1200, 3000);
+        background_tex_clouds2 = loadITextureRegion("bkgrnd_clouds.png", CAMERA_WIDTH, 3000);
 
-        background_tex_stars = loadITextureRegion("bkgrnd_stars.png", 1200, 3000);
+        background_tex_stars = loadITextureRegion("bkgrnd_stars.png", CAMERA_WIDTH, 3000);
 
         xWing_tex = loadITextureRegion("xwing_sprite.png", 200, 217);
 
@@ -168,8 +169,8 @@ public class Game extends SimpleBaseGameActivity implements IOnSceneTouchListene
         scene.attachChild(fpsText);
 
         //ENEMY MANAGEMENT
-        this.shotManager = new ShotManager( scene, this.getVertexBufferObjectManager() );
-        this.enemyManager = new EnemyManager( scene, this.getVertexBufferObjectManager(), this.shotManager );
+        this.enemyShotManager = new ShotManager( scene, this.getVertexBufferObjectManager() );
+        this.enemyManager = new EnemyManager( scene, this.getVertexBufferObjectManager(), this.enemyShotManager);
         enemyManager.addEnemyTexture(loadITextureRegion("tie_sprite_small.png", 200, 257), "tie_fighter");
 
         //Instantiate the player object
@@ -186,6 +187,9 @@ public class Game extends SimpleBaseGameActivity implements IOnSceneTouchListene
             }
         };
 
+        //SHOT MANAGMENT
+        this.playerShotManager = new ShotManager( scene, this.getVertexBufferObjectManager() );
+
         scene.attachChild(player);
 
         scene.registerUpdateHandler(new IUpdateHandler()
@@ -199,13 +203,14 @@ public class Game extends SimpleBaseGameActivity implements IOnSceneTouchListene
 
                 fpsText.setText(String.format("FPS: %.2f", fpsCounter.getFPS()));
                 player.update(v);
+                playerShotManager.update(v);
 
                 background_clouds1.updatePosition(v);
                 background_clouds2.updatePosition(v);
 
                 enemyManager.update(currentTime, v);
-                shotManager.update(v);
-            }
+                enemyShotManager.update(v);
+              }
 
             @Override
             public void reset() {}
